d9ff397b829b56062ebdab64875ed168e038cf73
Tyler Sisk <breadboardbasics@gmail.com>
2015-03-25 14:58:54 -0400

    All code working in rough state

--- Code/Lucid Programmer.bat
+++ Code/Lucid Programmer.bat
@@ -1,6 +1,10 @@
 @echo off
 TITLE Luc.id Programmer
 
+:::::::::::::::::::SET PROGRAMMER HERE:::::::::::::::::::::
+set programmer=usbtiny
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 set /p hour=Enter hour to wake up on (Military Time):
 set /p min=Enter minute to wake up on:
 set /p current=Enter tACS current (0-1500uA):
@@ -13,8 +17,6 @@ set new_current=%current%;//varCurrent
 set new_pdurr=%pdurr%;//varPdurr
 set new_npdurr=%npdurr%;//varNpdurr
 
-cd C:\Users\Tyler\Documents\Lucid\LucidC
-
 fart.exe -- Lucid.c //varHour %new_hour%
 fart.exe -- Lucid.c //varMin %new_min%
 fart.exe -- Lucid.c //varCurrent %new_current%
@@ -25,7 +27,6 @@ cls
 echo Compiling...
 
 avr-gcc -g -Os -mmcu=atmega328p -c Lucid.c
-if errorlevel 1 goto failed_compile
 avr-gcc -g -mmcu=atmega328p -o Lucid.elf Lucid.o
 avr-objcopy -j .text -j .data -O ihex Lucid.elf Lucid.hex
 
@@ -36,8 +37,7 @@ fart.exe -- Lucid.c %new_pdurr%  //varPdurr
 fart.exe -- Lucid.c %new_npdurr%  //varNpdurr
 
 cls
-
-avrdude -p atmega328p -c usbtiny -P -e -U flash:w:Lucid.hex
+avrdude -p atmega328p -c %programmer% -P -e -U flash:w:Lucid.hex
 if errorlevel 1 goto failed_upload
 
 cls
@@ -49,13 +49,11 @@ echo Pulse Durration = %pdurr% minutes
 echo Non-Pulse Durration = %npdurr% minutes 
 goto done
 
-:failed_compile
-echo Failed to compile, make sure that you have selected the right directory in the .bat file
-goto done
-
 :failed_upload
+cls
 echo Oops! Something is wrong please check programmer connections and try again.
-echo Also make sure that the correct programmer is selected in the .bat file (see README)
+echo Also make sure that the correct programmer is selected in the .bat file.
+echo See readme for more information
 
 :done
 pause
